b122edd0ca09418fffda9f61efa6072863700f41
shashi verma <shashikumarverma1996@gmail.com>
2025-03-18 13:51:58 +0530

    propmt improved

--- src/pages/api/propmt.ts
+++ src/pages/api/propmt.ts
@@ -2,5 +2,5 @@
 export const NumberOfQuestion:number=6
 export const createQuestions=(jobRole:string , jobDescription:string)=>
     
-     `search top 200 interview questions or question based on daily use and gibe ${NumberOfQuestion} in json format for the job role: "${jobRole} and job description ${jobDescription} ignore job jobDescription if it is irrelavent"`;
+     `search top 200 interview questions or question based on daily use give ${NumberOfQuestion} question in json format for the job role: "${jobRole} and job description ${jobDescription} ignore job jobDescription if it is irrelavent"`;
 
